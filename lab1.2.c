ddd2270d1c0e211de81f4adfb714d49ce1f282f8
RomanBegemaev <71429017+RomanBegemaev@users.noreply.github.com>
2020-10-24 00:05:50 +0700

    Update lab1.2.c

--- lab1.2.c
+++ lab1.2.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 int is_simple(int number)
 {
-    int num = 0;
-    if ((number == 0) || (number == 1))
+  int num=0;
+  if (number < 2)
+  {
+    printf("Wrong number ");
+    exit(0);
+  }
+  if ((number==0) || (number==1))
+  {
+    return 0;
+  }
+  else
+  {
+    for (int i=2;i<=number;i++)
     {
-        return 0;
+      if (number%i==0)
+      {
+        num++;
+      }
+    }
+    if (num>1)
+    {
+      return 0;
     }
     else
     {
-        for (int i = 2; i <= number; i++)
-        {
-            if (number % i == 0)
-            {
-                num++;
-            }
-        }
-        if (num > 1)
-        {
-            return 0;
-        }
-        else
-        {
-            return 1;
-        }
+      return 1;
     }
+  }
 }
-int main()
+int main ()
 {
-    int number;
-    scanf("%d", &number);
-    printf("%d", is_simple(number));
-    return 0;
+  int number;
+  scanf ("%d",&number);
+  printf ("%d",is_simple(number)); 
+  return 0;
 }
